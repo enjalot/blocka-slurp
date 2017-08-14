fe667eb7bc25d23bd5f3cb9e7b80775afcfbf57c
Micah Stubbs <micah.stubbs@gmail.com>
2017-08-14 14:00:24 -0700

    improve comment

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-# get a ids of all gists created by known users 
+# get the ids of all gists created by known users 
 # since a date we specify
+# write out the gist ids we find to data/latest.json
 coffee gist-meta.coffee data/latest.json 2016-10-01T00:00:00Z
 
 # download files for all gists in latest.json
@@ -7,5 +8,5 @@ coffee gist-cloner.coffee data/latest.json
 
 # generate a series of JSON files 
 # that pull out interesting metadata 
-# from the downloaded gists.
+# from the downloaded gists
 coffee parse.coffee
